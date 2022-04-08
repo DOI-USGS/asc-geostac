b5139688b949ced8411ca10cf2e633c3f3c7f250
Amy Stamile <74275278+amystamile-usgs@users.noreply.github.com>
2022-04-08 11:15:32 -0700

    Increase timeout for apply (#54)

--- app/src/components/presentational/SearchAndFilterInput.jsx
+++ app/src/components/presentational/SearchAndFilterInput.jsx
@@ -135,7 +135,7 @@ export default function SearchAndFilterInput(props) {
       setNumberReturned(getNumberReturned);
       setMaxNumberFootprints(getNumberMatched);
       props.footprintNavClick();
-    }, 1000);
+    }, 3000);
     setApplyChipVisStyle(css.chipHidden);
   }
 
