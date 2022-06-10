76c28a928b44cf2a64e7512756dafc446bbf00a5
Jay Laura <jlaura@usgs.gov>
2022-06-10 06:43:04 -0700

    testing using a path prefix for deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,12 @@
 const { merge } = require('webpack-merge');
 const Common = require("./webpack.common.js");
+const path = require('path');
 
 module.exports = merge(Common, {
   mode: "production",
   devtool: "source-map",
   watch: false,
+  output: {
+    publicPath: '/geostac/',
+  },
 });
