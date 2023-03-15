d5bb1b9290104f7115632b1becfc8984a203bcac
Jacob Cain <jrcain@usgs.gov>
2023-03-15 13:39:05 -0700

    Loading screen for footprints, message when none

--- src/components/presentational/FootprintResults.jsx
+++ src/components/presentational/FootprintResults.jsx
@@ -10,6 +10,7 @@ import PreviewIcon from "@mui/icons-material/Preview";
 import LaunchIcon from "@mui/icons-material/Launch";
 import OpenInFullIcon from "@mui/icons-material/OpenInFull";
 import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import TravelExploreIcon from '@mui/icons-material/TravelExplore'; // Footprints.]
 
 // object with results
 import { getFeatures } from "../../js/ApiJsonCollection";
@@ -17,6 +18,54 @@ import { getFeatures } from "../../js/ApiJsonCollection";
 // geotiff thumbnail viewer
 import DisplayGeoTiff from "../presentational/DisplayGeoTiff.jsx";
 import GeoTiffViewer from "../../js/geoTiffViewer.js";
+import { Skeleton } from "@mui/material";
+
+
+/**
+ * Skeleton to show when footprints are loading
+ */
+function LoadingFootprints() {
+  
+  return (
+    <div className="resultsList">
+      { Array(5).fill(null).map((_, i) => (
+        <div className="resultContainer" key={i}>
+          <div className="resultImgDiv">
+            <Skeleton variant="rectangular" width={32} height={32}/>
+          </div>
+          <div className="resultData">
+            <Skeleton/>
+            <Skeleton/>
+            <Skeleton/>
+            <Skeleton/>
+            <Skeleton/>
+          </div>
+          <div className="resultLinks">
+            <Stack direction="row" spacing={1} sx={{marginTop:1}}>
+              <Skeleton variant="rounded" width={100} height={20} sx={{borderRadius:5}}/>
+              <Skeleton variant="rounded" width={100} height={20} sx={{borderRadius:5}}/>
+            </Stack>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function NoFootprints(){
+  return(
+    <div style={{padding: 10, maxWidth: 268}}>
+      <p>
+        This target has no footprints. To see 
+        footprints, go to the dropdown menu 
+        in the upper left and pick a target 
+        body with the <TravelExploreIcon sx={{fontSize: 16, verticalAlign: "middle"}}/> icon next to it.
+      </p>
+    </div>
+  );
+}
+
+
 
 /**
  * Controls css styling for this component using js to css
@@ -45,14 +94,11 @@ export default function FootprintResults(props) {
 
   const [features, setFeatures] = React.useState([]);
 
-  const [footprintListComponent, setFootprintListComponent] = React.useState(() => {
-    return(
-      <div>Loading...</div>
-    );
-  })
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [hasFootprints, setHasFootprints] = React.useState(true);
 
+  // Metadata Popup
   const geoTiffViewer = new GeoTiffViewer("GeoTiffAsset");
-
   const showMetadata = (value) => () => {
     geoTiffViewer.displayGeoTiff(value.assets.thumbnail.href);
     geoTiffViewer.changeMetaData(
@@ -69,8 +115,14 @@ export default function FootprintResults(props) {
     // If target has collections (of footprints)
     if (props.target.collections.length > 0) {
 
+      // Set Loading
+      setIsLoading(true);
+      setHasFootprints(true);
+
+      // Promise tracking
       let fetchPromise = {};
       let jsonPromise = {};
+      // Result
       let jsonRes = {};
 
       let itemCollectionUrls = [];
@@ -104,13 +156,7 @@ export default function FootprintResults(props) {
       async function awaitFetch(targetUrl) {
         await fetchPromise[targetUrl];
         await jsonPromise[targetUrl];
-      }
-      
-      function extractFootprints(resultsArr) {
-        for(const result in resultsArr){
-
-        }
-      }
+      } 
 
       async function fetchAndWait() {
         // Start fetching
@@ -123,13 +169,12 @@ export default function FootprintResults(props) {
           await awaitFetch(itemCollectionUrl);
         }
         
+        // Extract footprints into array
         let resultsArr = [];
         let myFeatures = [];
-
         for(const itemCollectionUrl of itemCollectionUrls) {
           myFeatures.push(jsonRes[itemCollectionUrl]);
         }
-
         for(const featCollection of myFeatures) {
           resultsArr.push(...featCollection.features)
         }
@@ -137,32 +182,24 @@ export default function FootprintResults(props) {
         return resultsArr;
       }
 
-      
-
       (async () => {
         // Wait
         let myFeatures = await fetchAndWait()
         setFeatures(myFeatures);
-        setFootprintListComponent(
-          <>
-            <div>Footprints!</div>
-          </>
-        );
+        setHasFootprints(myFeatures.length > 0);
+        setIsLoading(false);
       })();
 
-      
-
-      
-
     } else {
-      setFootprintListComponent(<div>No footprints for this Target.</div>)
+      setIsLoading(false);
+      setHasFootprints(false);
     }
     
     // setTimeout(() => {
     //   setFeatures(getFeatures);
     // }, 1000); 
 
-  }, []);
+  }, [props.target.name]);
 
   return (
     <div style={css.root} className="scroll-parent">
@@ -182,50 +219,55 @@ export default function FootprintResults(props) {
           />
         </span>
       </div>
-      {footprintListComponent}
-      <div className="resultsList">
-        {features.map((feature) => (
-          <div className="resultContainer" key={feature.id}>
-            <div className="resultImgDiv">
-              <img className="resultImg" src={feature.assets.thumbnail.href} />
-            </div>
-            <div className="resultData">
-              <div className="resultSub">
-                <strong>Collection:</strong>&nbsp;{feature.collection}
+      {isLoading ? 
+        <LoadingFootprints/>
+      : hasFootprints ?   
+        <div className="resultsList">
+          {features.map((feature) => (
+            <div className="resultContainer" key={feature.id}>
+              <div className="resultImgDiv">
+                <img className="resultImg" src={feature.assets.thumbnail.href} />
               </div>
-              <div className="resultSub">
-                <strong>ID:</strong>&nbsp;{feature.id}
+              <div className="resultData">
+                <div className="resultSub">
+                  <strong>Collection:</strong>&nbsp;{feature.collection}
+                </div>
+                <div className="resultSub">
+                  <strong>ID:</strong>&nbsp;{feature.id}
+                </div>
+                <div className="resultSub">
+                  <strong>Date:</strong>&nbsp;{feature.properties.datetime}
+                </div>
               </div>
-              <div className="resultSub">
-                <strong>Date:</strong>&nbsp;{feature.properties.datetime}
+              <div className="resultLinks">
+                <Stack direction="row" spacing={1}>
+                  <Chip
+                    label="Metadata"
+                    icon={<PreviewIcon />}
+                    size="small"
+                    onClick={showMetadata(feature)}
+                    variant="outlined"
+                    clickable
+                  />
+                  <Chip
+                    label="STAC Browser"
+                    icon={<LaunchIcon />}
+                    size="small"
+                    component="a"
+                    href={`https://stac.astrogeology.usgs.gov/browser-dev/#/collections/${feature.collection}/items/${feature.id}`}
+                    target="_blank"
+                    //href="https://stac.astrogeology.usgs.gov/browser-dev/"
+                    variant="outlined"
+                    clickable
+                  />
+                </Stack>
               </div>
             </div>
-            <div className="resultLinks">
-              <Stack direction="row" spacing={1}>
-                <Chip
-                  label="Metadata"
-                  icon={<PreviewIcon />}
-                  size="small"
-                  onClick={showMetadata(feature)}
-                  variant="outlined"
-                  clickable
-                />
-                <Chip
-                  label="STAC Browser"
-                  icon={<LaunchIcon />}
-                  size="small"
-                  component="a"
-                  href={`https://stac.astrogeology.usgs.gov/browser-dev/#/collections/${feature.collection}/items/${feature.id}`}
-                  target="_blank"
-                  //href="https://stac.astrogeology.usgs.gov/browser-dev/"
-                  variant="outlined"
-                  clickable
-                />
-              </Stack>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      :
+        <NoFootprints/>
+      }
     </div>
   );
 }
